5c2e5ae39355c00169471e36a01a724f4a1362a5
Richill Tamakloe <richill.tamakloe@gmail.com>
2020-01-08 18:11:52 +0000

    created method to add value in input field within form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -5,7 +5,26 @@ export default class TodoForm extends React.Component {
     text: ""
   }
 
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    // submit form
+  }
+
   render() {
-    return <input/>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input 
+          name="text"
+          value={this.state.text} 
+          onChange={this.handleChange}
+          placeholder="todo..."/>
+      </form>
+    );
   }
 }
