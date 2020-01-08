a469ac5967844455f373329e8a6e3ec2b055c918
Richill Tamakloe <richill.tamakloe@gmail.com>
2020-01-08 02:11:47 +0000

    updated class App in app.js to display created todo js file in components

---
+++ src/components/TodoList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+/*
+Task: TodoMVC
+1. add todos
+2. display todos
+3. cross off todos
+4. show number of active todos
+5. filter all/active/complete
+6. delete all complete
+8. button to toggle all on/off
+*/
+
+export default class TodoList extends React.Component {
+  render() {
+    return <div>todos...</div>
+  }
+}
